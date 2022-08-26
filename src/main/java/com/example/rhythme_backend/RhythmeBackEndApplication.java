71e8e9e44ba5c5630de2fd61ea099b5b3f6c39c8
ericjjung <ericjung@github.com>
2022-08-26 23:21:58 +0900

    start project

---
+++ src/main/java/com/example/rhythme_backend/RhythmeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhythme_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhythmeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rhythme_backend/RhythmeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhythme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhythmeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
