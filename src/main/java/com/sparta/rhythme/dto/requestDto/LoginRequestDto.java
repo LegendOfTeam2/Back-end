c857adb669da70a429ecd4f44f4ba4a0188798c4
ericjjung <ericjung@github.com>
2022-08-30 00:01:18 +0900

    delete :  패키지명 다른 파일들 삭제

--- src/main/java/com/sparta/rhythme/dto/requestDto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.rhythme.dto.requestDto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginRequestDto {
-    private String email;
-    private String password;
-}
--- src/main/java/com/sparta/rhythme/util/Authority.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.rhythme.util;
-
-public enum Authority {
-    ROLE_MEMBER,
-    ROLE_GUEST
-}
