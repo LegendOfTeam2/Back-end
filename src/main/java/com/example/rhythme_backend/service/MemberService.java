714760b68582fef0b46ad42b58c89eab28682089
wkddnjs <wkddnjs0416@gmail.com>
2022-09-02 13:51:38 +0900

    Chore : MemberService의 카카오 매서드 이관

--- src/main/java/com/example/rhythme_backend/service/MemberService.java
+++ src/main/java/com/example/rhythme_backend/service/MemberService.java
@@ -15,10 +15,7 @@ import com.example.rhythme_backend.repository.RefreshTokenRepository;
 import com.example.rhythme_backend.service.kakaoLogin.KakaoOauth;
 import com.example.rhythme_backend.util.Message;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,9 +24,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -182,75 +176,6 @@ public class MemberService {
         return tokenDto;
 
     }
-
-//    @Value("${myKaKaoRestAplKey}")
-//    private String myKaKaoRestAplKey;
-//
-//    private String getAccessToken(String code) throws JsonProcessingException {
-//
-//        // HTTP Header 생성
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//        // HTTP Body 생성
-//        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-//        body.add("grant_type", "authorization_code");
-//        body.add("client_id", myKaKaoRestAplKey);
-//        body.add("redirect_uri", "http://localhost:3000/kakao/callback");
-//        body.add("code", code);
-//
-//        // HTTP 요청 보내기
-//        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-//                new HttpEntity<>(body, headers);
-//        RestTemplate rt = new RestTemplate();
-//        ResponseEntity<String> response = rt.exchange(
-//                "https://kauth.kakao.com/oauth/token",
-//                HttpMethod.POST,
-//                kakaoTokenRequest,
-//                String.class
-//        );
-//
-//        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
-//        String responseBody = response.getBody();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = objectMapper.readTree(responseBody);
-//        return jsonNode.get("access_token").asText();
-//    }
-
-//    private KakaoUserInfoDto getKakaoUserInfo(String accessToken) throws JsonProcessingException {
-//
-//        // HTTP Header 생성
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Authorization", "Bearer " + accessToken);
-//        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//        // HTTP 요청 보내기
-//        HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
-//        RestTemplate rt = new RestTemplate();
-//        ResponseEntity<String> response = rt.exchange(
-//                "https://kapi.kakao.com/v2/user/me",
-//                HttpMethod.POST,
-//                kakaoUserInfoRequest,
-//                String.class
-//        );
-//
-//        String responseBody = response.getBody();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = objectMapper.readTree(responseBody);
-//        Long id = jsonNode.get("id").asLong();
-//        String nickname = jsonNode.get("properties")
-//                .get("nickname").asText();
-//        String email = jsonNode.get("kakao_account")
-//                .get("email").asText();
-//
-//
-//        System.out.println("카카오 사용자 정보: " + id + ", " + nickname + ", " + email);
-//        return KakaoUserInfoDto.builder()
-//                .kakaoid(id)
-//                .email(email)
-//                .nickname(nickname)
-//                .build();
-//    }
 
     //google
     @Transactional
