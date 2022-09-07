2d2835cccb995c84e497e50ff62b07c2feb3bda1
ericjjung <ericjung@github.com>
2022-09-07 16:12:34 +0900

    Chore : profile controller

---
+++ src/main/java/com/example/rhythme_backend/controller/ProfilePageController.java
@@ -0,0 +1,11 @@
+package com.example.rhythme_backend.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfilePageController {
+    
+}
