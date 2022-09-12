92e5a7657a4faad16c6aaffc53f247f2a8043a97
rokga <pk123408@naver.com>
2022-09-12 21:14:05 +0900

    feat:  게시글 좋아요 기능 수정 .   기존에는 Singer / Maker 로 구분해서 각각의 api를 사용했는데 통합 하는 걸로 수정.( postman test 는 완료)

--- src/main/java/com/example/rhythme_backend/controller/LikeController.java
+++ src/main/java/com/example/rhythme_backend/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.example.rhythme_backend.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,10 +15,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LikeController {
     private final LikeService likeService;
 
-    @PostMapping("/auth/singerpost/{postId}/like")
-    public ResponseDto<?> SingerLike(@PathVariable Long postId, HttpServletRequest request){
-        return likeService.upDownSingerLike(postId, request);
-    }
+    @PostMapping("/auth/post/{postId}/like")
+    public ResponseDto<?> SingerLike(@PathVariable Long postId, HttpServletRequest request,
+                                    @RequestParam String position){
+        if(position.equals("Singer")){
+            return likeService.upDownSingerLike(postId,request);
+        }
+        if(position.equals("Maker")){
+            return likeService.upDownMakerLike(postId,request);
+        }
+        return ResponseDto.success("잘못된 접근입니다");
+        }
 
     @PostMapping("/auth/makerpost/{postId}/like")
     public ResponseDto<?> MakerLike(@PathVariable Long postId, HttpServletRequest request){
