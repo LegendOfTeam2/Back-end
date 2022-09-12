795ca69efa3587ed9c11fd034d28d83e91108fd3
rokga <pk123408@naver.com>
2022-09-12 16:19:20 +0900

    CI: 서버 오류로 인한 커밋

--- src/main/java/com/example/rhythme_backend/controller/PostController.java
+++ src/main/java/com/example/rhythme_backend/controller/PostController.java
@@ -47,6 +47,7 @@ public class PostController {
     @GetMapping("/allpost/search")
     public ResponseEntity<?> searchAllPost(@RequestParam(required = false, defaultValue = "") String searchText,
                                            @RequestParam String category) {
+        
         return postService.AllPostSearch(searchText,category);
     }
 }
