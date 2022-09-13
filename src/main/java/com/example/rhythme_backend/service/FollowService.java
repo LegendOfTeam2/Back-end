ea4720ea7c79794edc6368a833a8224969ce57e0
rokga <pk123408@naver.com>
2022-09-13 10:38:19 +0900

    fix:  팔로우기능 본인 팔로우 하는 로직 삭제

--- src/main/java/com/example/rhythme_backend/service/FollowService.java
+++ src/main/java/com/example/rhythme_backend/service/FollowService.java
@@ -3,12 +3,10 @@ package com.example.rhythme_backend.service;
 import com.example.rhythme_backend.domain.Follow;
 import com.example.rhythme_backend.domain.Member;
 import com.example.rhythme_backend.dto.requestDto.FollowRequestDto;
-import com.example.rhythme_backend.exception.CustomException;
-import com.example.rhythme_backend.exception.ErrorCode;
-import com.example.rhythme_backend.jwt.TokenProvider;
 import com.example.rhythme_backend.repository.FollowRepository;
 import com.example.rhythme_backend.repository.MemberRepository;
 import com.example.rhythme_backend.util.ResponseDto;
+import com.example.rhythme_backend.util.Validation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,17 +19,16 @@ import java.util.Optional;
 @Slf4j
 public class FollowService {
     private final FollowRepository followRepository;
-    private final TokenProvider tokenProvider;
+//    private final TokenProvider tokenProvider;
     private final MemberRepository memberRepository;
+    private final Validation validation;
 
     public ResponseDto<?> upDownFollow(String nickname, HttpServletRequest request) {
-        Member follower = validateMember(request);
-        checkAccessToken(request, follower);
-        Member following = isPresentMemberFollow(nickname);
+        Member follower = validation.validateMember(request);
+        validation.checkAccessToken(request, follower);
+        Member following = validation.isPresentMemberFollow(nickname);
         Optional<Follow> findFollowing = followRepository.findByFollowerAndFollowing(follower, following);
-        if(follower.getNickname().equals(following.getNickname())){
-            return ResponseDto.success("본인을 팔로우 할 수 없습니다 ");
-        }
+        
         if(findFollowing.isEmpty()) {
             FollowRequestDto followRequestDto = new FollowRequestDto(follower, following);
             Follow follow = new Follow(followRequestDto);
@@ -59,7 +56,6 @@ public class FollowService {
 //        }
 //        return new ResponseEntity<>(Message.success(followedResponseDtoList), HttpStatus.OK);
 //    }
-
 //    @Transactional(readOnly = true)
 //    public ResponseEntity<?> getMemberByAllFoloowing(String nickname , HttpServletRequest request){
 //        validateMember(request);
@@ -70,24 +66,6 @@ public class FollowService {
 //            followedResponseDtoList.add(new FollowedResponseDto(follw))
 //        }
 //    }
-    public Member validateMember(HttpServletRequest request) {
-        if (!tokenProvider.validateToken(request.getHeader("Authorization").substring(7))) {
-            return null;
-        }
-        return tokenProvider.getMemberFromAuthentication();
-    }
-
-        public void checkAccessToken (HttpServletRequest request, Member member){
-            if (!tokenProvider.validateToken(request.getHeader("Authorization").substring(7)))
-                throw new CustomException(ErrorCode.TOKEN_IS_EXPIRED);
-            if (null == member) throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
-        }
-    public Member isPresentMemberFollow(String  nickname) {
-        Optional<Member> optionalMember = memberRepository.findByNickname(nickname);
-        return optionalMember.orElseThrow(
-                () -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)
-        );
-    }
 }
 
 
