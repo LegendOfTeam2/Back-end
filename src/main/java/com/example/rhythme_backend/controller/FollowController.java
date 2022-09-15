fd479b951c61d97355c971354a643d52e95796df
wkddnjs <wkddnjs0416@gmail.com>
2022-09-15 23:34:01 +0900

    Chore: Get-->Post 매핑 수정

--- src/main/java/com/example/rhythme_backend/controller/FollowController.java
+++ src/main/java/com/example/rhythme_backend/controller/FollowController.java
@@ -5,6 +5,7 @@ import com.example.rhythme_backend.util.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 public class FollowController {
     private final FollowService followService;
 
-    @GetMapping("/auth/follow/{nickname}")
+    @PostMapping("/auth/follow/{nickname}")
     public ResponseDto<?> follow(@PathVariable String nickname, HttpServletRequest request) {
         return followService.upDownFollow(nickname, request);
     }
