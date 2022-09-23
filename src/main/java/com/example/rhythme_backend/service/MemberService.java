80f32f2ce984020006614bf31d0c2bf1a666b4f4
wkddnjs <wkddnjs0416@gmail.com>
2022-09-24 00:46:58 +0900

    Fix: dev-2 확인용 커밋

--- src/main/java/com/example/rhythme_backend/service/MemberService.java
+++ src/main/java/com/example/rhythme_backend/service/MemberService.java
@@ -265,7 +265,6 @@ public class MemberService {
                             .build());
         }
     }
-
 //    리펙토링 할 때 사용하기 (삭제ㄴㄴ)
 //    @Transactional(readOnly = true)
 //    public Member getPresentEmail(String email) {
